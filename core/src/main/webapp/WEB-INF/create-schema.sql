81266599e523b4570034db1272979b5d8d9ddecd
hemasundar <hemasundar@sageabletech.com>
2018-01-23 11:39:28 +0530

    Updated sql schema

--- core/src/main/webapp/WEB-INF/create-schema.sql
+++ core/src/main/webapp/WEB-INF/create-schema.sql
@@ -28,6 +28,7 @@ CREATE TABLE `branch_counter` (
   `phone` varchar(100) NOT NULL,
   `active` tinyint(1) NOT NULL,
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
   PRIMARY KEY (`password`),
   UNIQUE KEY `branch_id` (`branch_id`,`phone`),
   UNIQUE KEY `id_UNIQUE` (`id`),
@@ -178,4 +179,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-19 15:13:36
+-- Dump completed on 2018-01-23 11:37:42
