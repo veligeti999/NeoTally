77f2f1522a52779e35ba5bbc18f2204f1d49e161
hemasundar <hemasundar@sageabletech.com>
2018-02-13 17:50:47 +0530

    added cors to options

--- core/src/main/java/com/newtally/core/resource/CORSResponseFilter.java
+++ core/src/main/java/com/newtally/core/resource/CORSResponseFilter.java
@@ -3,6 +3,9 @@ import javax.ws.rs.ext.Provider;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 
 @Provider
 public class CORSResponseFilter implements ContainerResponseFilter {
@@ -14,6 +17,13 @@ public void filter(ContainerRequestContext creq, ContainerResponseContext cres) 
     cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
     cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     cres.getHeaders().add("Access-Control-Max-Age", "");
+    
+    if ("OPTIONS".equalsIgnoreCase(creq.getMethod())) {
+        ResponseBuilder responseBuilder = Response.status(Status.ACCEPTED);
+        Response response = responseBuilder.status(Status.ACCEPTED).build();
+        creq.abortWith(response);
+        return;
+    } 
 }
 
 }
